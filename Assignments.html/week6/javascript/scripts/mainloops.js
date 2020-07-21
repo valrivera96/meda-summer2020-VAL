754d0024676fadce1704182d2a26a4a3eb8590fc
valrivera <vmrarriola@gmail.com>
2020-07-21 13:00:01 -0700

    message

---
+++ Assignments.html/week6/javascript/scripts/mainloops.js
@@ -0,0 +1,19 @@
+// The If Statement 
+
+var number1 = 10;
+var number2 = 10;
+
+// Syntax
+// if (conditional) { group of code to run if condtional is true}
+
+
+
+// Logical Operators
+// == Compares two values, becomes the boolean true if it is the same, and false if they are not the same.
+
+
+if (number1 == number2) {
+    console.log("This if statement was true so I ran this code!");
+}
+
+console.log("Program Finished!");
